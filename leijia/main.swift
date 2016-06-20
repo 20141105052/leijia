f36675420a2a61f25bfd84c343a10d18b9864a73
20141105052 <xiaoods3on1@sina.com>
2016-06-20 09:57:16 +0800

    Initial Commit

---
+++ leijia/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leijia
+//
+//  Created by 20141105052 on 16/6/20.
+//  Copyright (c) 2016年 20141105052郭孝宇. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
